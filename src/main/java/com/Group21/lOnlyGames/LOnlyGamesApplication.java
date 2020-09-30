17fa794bf82681ba256cc65e28bd90bd157c86ad
Brendon Lam <brendonlam123@hotmail.com>
2020-09-30 19:58:44 +1000

    Initial Commit
    Needs Docker Setup, won't work now unless the DB is setup and turned on etc.

---
+++ src/main/java/com/Group21/lOnlyGames/LOnlyGamesApplication.java
@@ -0,0 +1,13 @@
+package com.Group21.lOnlyGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LOnlyGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LOnlyGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Group21/lOnlyGames/LOnlyGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Group21.lOnlyGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LOnlyGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
