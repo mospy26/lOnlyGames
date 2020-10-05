1f0e1f1a8a5e0030c94a1a5906516bdd5b4e0f25
Nabeel Khan <nabeelmahmoodkhan@gmail.com>
2020-10-06 01:28:46 +1100

    Created front end template

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+function Footer(){
+
+    return (
+        <footer className='footer'>
+            <p className='footer__content' >&copy; Copyright 2020 lOnlyGames | All rights reserved.</p>
+        </footer>
+    );
+}export default Footer;
---
+++ frontend/src/components/Matches.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header'
+import Footer from './Footer'
+
+function Matches() {
+    return (
+        <div>
+            <Header />
+            <h1>Matches page</h1>
+            <Footer />
+        </div>
+    );
+};
+
+export default Matches;
