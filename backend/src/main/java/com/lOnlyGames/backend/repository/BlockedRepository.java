3182d35b254adb655828b70cb3f4c198290c72fd
Brendon Lam <brendonlam123@hotmail.com>
2020-10-05 16:53:07 +1100

    Added additional methods to the Repository files

--- backend/src/main/java/com/lOnlyGames/backend/repository/BlockedRepository.java
+++ backend/src/main/java/com/lOnlyGames/backend/repository/BlockedRepository.java
@@ -2,7 +2,11 @@ package com.lOnlyGames.backend.repository;
 
 import com.lOnlyGames.backend.model.Availability;
 import com.lOnlyGames.backend.model.Blocked;
+import com.lOnlyGames.backend.model.User;
+import org.springframework.cglib.core.Block;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BlockedRepository extends CrudRepository<Blocked, Integer> {
+    public Blocked findByBlocker(User user);
+    public Blocked findByBlockee(User user);
 }
