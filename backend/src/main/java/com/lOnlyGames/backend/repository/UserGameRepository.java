e1e7ca78be1344aae512dc8fc0822a54a236f5a8
Brendon Lam <brendonlam123@hotmail.com>
2020-10-05 17:46:48 +1100

    Added Methods to Repositories UserAvailabilityRepository.java and UserGameRepository.java

--- backend/src/main/java/com/lOnlyGames/backend/repository/UserGameRepository.java
+++ backend/src/main/java/com/lOnlyGames/backend/repository/UserGameRepository.java
@@ -2,8 +2,13 @@ package com.lOnlyGames.backend.repository;
 
 import com.lOnlyGames.backend.model.Availability;
 import com.lOnlyGames.backend.model.CompositeKeys.UserGameCK;
+import com.lOnlyGames.backend.model.Game;
+import com.lOnlyGames.backend.model.User;
 import com.lOnlyGames.backend.model.UserGame;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserGameRepository extends CrudRepository<UserGame, UserGameCK> {
+    public UserGame findByGameRank(String rank);
+    public UserGame findByGame(Game game);
+    public UserGame findByUser(User user);
 }
