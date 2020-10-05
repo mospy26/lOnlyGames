dbe704192d42c7e8dc5161d30f16cbdcdf6d1aef
Brendon Lam <brendonlam123@hotmail.com>
2020-10-05 15:49:53 +1100

    Added Repositories

---
+++ backend/src/main/java/com/lOnlyGames/backend/repository/AvailabilityRepository.java
@@ -0,0 +1,7 @@
+package com.lOnlyGames.backend.repository;
+
+import com.lOnlyGames.backend.model.Availability;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AvailabilityRepository extends CrudRepository<Availability, Integer> {
+}
