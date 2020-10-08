e29358d7946428d8f31b2a33a62265f827aeabe8
Brendon Lam <brendonlam123@hotmail.com>
2020-10-08 18:02:29 +1100

    Base Home Page Template

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header'
 import Footer from './Footer'
+import logo from '../resources/logo.png'
+import "../styles/Home.css"
+import styled from 'styled-components'
 
-function Home(props) {
-    return (
-        <div>
-            <Header />
-            <h1>Replace this tag with Home Page components</h1>
-            <Footer />
-        </div>
-    );
+class Home extends Component {
+
+    render() {
+        return(
+            <div>
+                {/* <Header /> */}
+                <img 
+                    src={logo}
+                    alt="logo"
+                    className="logo-img" 
+                />
+
+                <div className="banner-text">
+                    <h1>Welcome to lOnlyGames</h1>
+                    <p>
+                        lOnlyGames is a social gaming collaboration platform, 
+                        aimed at connecting users with similar gaming interests. 
+                        It is a platform that allows a user to find other players based on characteristics such as games played, 
+                        type of gamer, skills in a particular game, location and availability
+                    </p>
+                </div>
+
+                <div className="button-container">
+                    <button>Sign In</button>
+                    <button>Sign Up</button>
+                </div>
+
+                <Footer />
+            </div>
+        )
+    }
 }
 
 export default Home;
