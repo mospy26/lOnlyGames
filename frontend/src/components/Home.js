2932214ad22cd3987b97c4598c6c002eba4e6eef
Brendon Lam <brendonlam123@hotmail.com>
2020-10-09 15:32:48 +1100

    Basic Look

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
 import Header from './Header'
 import Footer from './Footer'
-import logo from '../resources/logo.png'
+import logo from '../resources/logo-photo-with-text.png'
 import "../styles/Home.css"
-import styled from 'styled-components'
+import {NavLink} from 'react-router-dom'
 
 class Home extends Component {
 
     render() {
         return(
-            <div>
+            <div className="background">
                 {/* <Header /> */}
-                <img 
-                    src={logo}
-                    alt="logo"
-                    className="logo-img" 
-                />
+                <img src={logo} alt="logo" className="logo-img"/>
 
                 <div className="banner-text">
-                    <h1>18+ ONLY</h1>
+                    <h1>Welcome to lOnlyGames</h1>
                     <p>
                         lOnlyGames is a social gaming collaboration platform, 
                         aimed at connecting users with similar gaming interests. 
@@ -28,8 +24,12 @@ class Home extends Component {
                 </div>
 
                 <div className="button-container">
-                    <button>Sign In</button>
-                    <button>Sign Up</button>
+                    <NavLink to='/about' exact activeClassName='active'>
+                        <button>Sign In</button>
+                    </NavLink>
+                    <NavLink to='/profile' exact activeClassName='active'>
+                        <button>Sign Up</button>
+                    </NavLink>
                 </div>
 
                 <Footer />
---
+++ frontend/src/styles/Home.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+    bac: palevioletred;
+    border-radius: 3px;
+    border: none;
+    color: white;
+`
