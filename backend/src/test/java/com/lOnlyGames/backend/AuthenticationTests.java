0c0f0973db902616f8e8ff6f0d3e903ea713aa94
mospy26 <m.mustafaahmed@hotmail.com>
2020-10-23 20:30:03 +1100

    Added more tests for authentication

--- backend/src/test/java/com/lOnlyGames/backend/AuthenticationTests.java
+++ backend/src/test/java/com/lOnlyGames/backend/AuthenticationTests.java
@@ -1,10 +1,12 @@
 package com.lOnlyGames.backend;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 import com.lOnlyGames.backend.auth.JwtTokenUtil;
 import com.lOnlyGames.backend.errorhandlers.exceptions.InvalidCredentialsException;
+import com.lOnlyGames.backend.errorhandlers.exceptions.InvalidUsernameException;
 import com.lOnlyGames.backend.model.User;
 import com.lOnlyGames.backend.response.JwtTokenResponse;
 import com.lOnlyGames.backend.services.UserService;
@@ -45,7 +47,6 @@ public class AuthenticationTests {
         when(userService.loadUserByUsername("username")).thenReturn(testUser);
         when(jwtTokenUtil.generateToken(testUser)).thenReturn("TOKENN");
         ResponseEntity<JwtTokenResponse> responseEntity = (ResponseEntity<JwtTokenResponse>) authenticationController.login(testUser);
-        System.out.println(responseEntity.getBody().getToken());
 
         assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
         assertThat(responseEntity.getBody().getToken()).isEqualTo("TOKENN");
@@ -61,6 +62,36 @@ public class AuthenticationTests {
         when(userService.authenticate("invalid username", "password")).thenThrow(new InvalidCredentialsException());
         Assertions.assertThrows(InvalidCredentialsException.class, () -> {
             authenticationController.login(testUser);
+        });
+    }
+
+    @Test
+    public void testControllerRegisterWithValidCreds() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+
+        User testUser = new User("username");
+        testUser.setPassword("password");
+
+        when(userService.loadUserByUsername("username")).thenReturn(testUser);
+        when(jwtTokenUtil.generateToken(testUser)).thenReturn("TOKENN");
+        ResponseEntity<JwtTokenResponse> responseEntity = (ResponseEntity<JwtTokenResponse>) authenticationController.register(testUser);
+
+        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
+        assertThat(responseEntity.getBody().getToken()).isEqualTo("TOKENN");
+    }
+
+    @Test
+    public void testControllerRegisterWithInvalidUserCredsReturnsResponseEntity() throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+
+        User testUser = new User("invalid username");
+        testUser.setPassword("password");
+        doThrow(new InvalidUsernameException()).when(userService).register(testUser);
+
+        Assertions.assertThrows(InvalidUsernameException.class, () -> {
+            authenticationController.register(testUser);
         });
     }
 }
