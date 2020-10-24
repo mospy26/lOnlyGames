48cdd1df5a5dd0efb4ceaaf6a831f840b857d02a
Brendon Lam <brendonlam123@hotmail.com>
2020-10-24 12:46:05 +1100

    looks nicer lol

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,35 +4,33 @@ import Footer from './Footer'
 import logo from '../resources/logo-photo-with-text.png'
 import "../styles/Home.css"
 import {NavLink} from 'react-router-dom'
+import YoutubeBackground from 'react-youtube-background'
 
 const Home = () => {
     return(
         <React.Fragment>
-            <div className="background">
-                {/* <Header /> */}
-                <img src={logo} alt="logo" className="logo-img"/>
+            <YoutubeBackground videoId={"lRTtMcx6rSM"}>
+                <div className="background" id="background-spacing">
+                    {/* <Header /> */}
+                    <img src={logo} alt="logo" className="logo-img"/>
 
-                <div className="banner-text">
-                    <h1>Welcome to lOnlyGames</h1>
-                    <p>
-                        lOnlyGames is a social gaming collaboration platform, 
-                        aimed at connecting users with similar gaming interests. 
-                        It is a platform that allows a user to find other players based on characteristics such as games played, 
-                        type of gamer, skills in a particular game, location and availability
-                    </p>
+                    <div className="banner-text">
+                        <h1>Match with other gamers today!</h1>
+                    </div>
+
+                    <div className="button-container">
+                        <NavLink to='/about' className='button' exact activeClassName='active'>
+                            <button>Sign In</button>
+                        </NavLink>
+                        <NavLink to='/profile' className='button' exact activeClassName='active'>
+                            <button>Sign Up</button>
+                        </NavLink>
+                    </div>
+
+                    {/* <Footer /> */}
                 </div>
+            </YoutubeBackground>
 
-                <div className="button-container">
-                    <NavLink to='/about' exact activeClassName='active'>
-                        <button>Sign In</button>
-                    </NavLink>
-                    <NavLink to='/profile' exact activeClassName='active'>
-                        <button>Sign Up</button>
-                    </NavLink>
-                </div>
-
-                <Footer />
-            </div>
         </React.Fragment>
         
     )
