ae172229f92a8c5ed4b810d9e05a58a19e6cf8fc
mospy26 <m.mustafaahmed@hotmail.com>
2020-10-26 14:04:46 +1100

    WIP profile page rebased with frontend

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -2,11 +2,75 @@ import React from 'react';
 import Header from './Header'
 import Footer from './Footer'
 
-function Profile(){
+function Profile() {
     return (
         <div>
             <Header />
-            <h1>Profile page</h1>
+            <div class="profile-page">
+                <div class="profile-card">
+                    <img src="favicon.ico" alt="John" style={{ width: 100 }} />
+                    <h1>John Doe</h1>
+                    <p class="title">DiscordID: abcd</p>
+                    <p>SteamID: abcd</p>
+                </div>
+
+                <div class="profile-items">
+                    <table class="profile-table">
+                        <tr class="profile-row">
+                            <td>Name</td>
+                            <td> Namememe </td>
+                        </tr>
+                        <tr class="profile-row">
+                            <td>Games Played</td>
+                            <td> hhahaha </td>
+                        </tr>
+                        <tr class="profile-row">
+                            <td>Games Played</td>
+                            <td> hhahaha </td>
+                        </tr>
+                        <tr class="profile-row">
+                            <td>Games Played</td>
+                            <td> hhahaha </td>
+                        </tr>
+                        <tr class="profile-row">
+                            <td>Games Played</td>
+                            <td> hhahaha </td>
+                        </tr>
+                        <tr class="profile-row">
+                            <td>Games Played</td>
+                            <td> hhahaha </td>
+                        </tr>
+                        <tr class="profile-row">
+                            <td>Games Played</td>
+                            <td> hhahaha </td>
+                        </tr>
+                        <tr class="profile-row">
+                            <td>Games Played</td>
+                            <td> hhahaha </td>
+                        </tr>
+                        <tr class="profile-row">
+                            <td>Games Played</td>
+                            <td> hhahaha </td>
+                        </tr>
+                        <tr class="profile-row">
+                            <td>Games Played</td>
+                            <td> hhahaha </td>
+                        </tr>
+                        <tr class="profile-row">
+                            <td>Games Played</td>
+                            <td> hhahaha </td>
+                        </tr>
+                        <tr class="profile-row">
+                            <td>Games Played</td>
+                            <td> hhahaha </td>
+                        </tr>
+                        <tr class="profile-row">
+                            <td>Games Played</td>
+                            <td> hhahaha </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
             <Footer />
         </div>
     );
