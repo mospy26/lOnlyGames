a58965d805bb74da248e9946d406bf7f274fd23d
mospy26 <m.mustafaahmed@hotmail.com>
2020-10-29 01:47:45 +1100

    Added cors annotation

--- backend/src/main/java/com/lOnlyGames/backend/controllers/AuthenticationController.java
+++ backend/src/main/java/com/lOnlyGames/backend/controllers/AuthenticationController.java
@@ -12,12 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping(path="/api/v1/auth")
 public class AuthenticationController {
     
