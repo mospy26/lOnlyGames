7835099c5569e0941bdfa7d87e5226bad7f37dc4
mbaki98 <69335110+mbaki98@users.noreply.github.com>
2020-11-04 09:59:28 +1100

    Added methods to add and remove Availabilities and UserAvailabilities

--- backend/src/main/java/com/lOnlyGames/backend/DAO/AvailabilityDAO.java
+++ backend/src/main/java/com/lOnlyGames/backend/DAO/AvailabilityDAO.java
@@ -1,5 +1,7 @@
 package com.lOnlyGames.backend.DAO;
 
+import com.lOnlyGames.backend.model.Availability;
+import com.lOnlyGames.backend.model.UserAvailability;
 import com.lOnlyGames.backend.repository.AvailabilityRepository;
 import com.lOnlyGames.backend.repository.UserAvailabilityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +21,21 @@ public class AvailabilityDAO {
 
     public UserAvailabilityRepository getUserAvailabilityRepository() {
         return userAvailabilityRepository;
+    }
+
+    public void addAvailability(Availability availability) {
+        availabilityRepository.save(availability);
+    }
+
+    public void deleteAvailability(Availability availability) {
+        availabilityRepository.delete(availability);
+    }
+
+    public void addUserAvailability(UserAvailability userAvailability) {
+        userAvailabilityRepository.save(userAvailability);
+    }
+
+    public void deleteUserAvailability(UserAvailability userAvailability) {
+        userAvailabilityRepository.delete(userAvailability);
     }
 }
