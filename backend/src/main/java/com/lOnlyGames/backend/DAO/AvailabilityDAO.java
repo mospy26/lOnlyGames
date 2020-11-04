4faaa2bdd7ee44a0fbb214b95208885173325a8b
mbaki98 <69335110+mbaki98@users.noreply.github.com>
2020-11-04 11:56:51 +1100

    Added getAvailability and getUseravailability methods to DAO

--- backend/src/main/java/com/lOnlyGames/backend/DAO/AvailabilityDAO.java
+++ backend/src/main/java/com/lOnlyGames/backend/DAO/AvailabilityDAO.java
@@ -1,11 +1,15 @@
 package com.lOnlyGames.backend.DAO;
 
 import com.lOnlyGames.backend.model.Availability;
+import com.lOnlyGames.backend.model.CompositeKeys.UserAvailabilityCK;
+import com.lOnlyGames.backend.model.User;
 import com.lOnlyGames.backend.model.UserAvailability;
 import com.lOnlyGames.backend.repository.AvailabilityRepository;
 import com.lOnlyGames.backend.repository.UserAvailabilityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class AvailabilityDAO {
@@ -37,5 +41,17 @@ public class AvailabilityDAO {
 
     public void deleteUserAvailability(UserAvailability userAvailability) {
         userAvailabilityRepository.delete(userAvailability);
+    }
+
+    public Availability getAvailability(Integer id) {
+        Optional<Availability> availability = availabilityRepository.findById(id);
+        if (!availability.isPresent()) return null;
+        return availability.get();
+    }
+
+    public UserAvailability getUserAvailability(UserAvailabilityCK compositeKey) {
+        Optional<UserAvailability> userAvailability = userAvailabilityRepository.findById(compositeKey);
+        if (!userAvailability.isPresent()) return null;
+        return userAvailability.get();
     }
 }
