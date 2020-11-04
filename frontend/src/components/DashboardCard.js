56eaaa2ce064c98f1f3bc3bbbe0be5b09e4f6727
mospy26 <m.mustafaahmed@hotmail.com>
2020-11-04 23:11:36 +1100

    React tinder library used, images not loading

--- frontend/src/components/DashboardCard.js
+++ frontend/src/components/DashboardCard.js
@@ -1,26 +1,87 @@
-import React, { Component } from 'react';
+import React, { useState, useMemo, Component } from 'react';
 import "../styles/DashboardCard.css";
+import TinderCard from 'react-tinder-card'
+import Header from './Header'
+import Footer from './Footer'
+
+const db = [
+    {
+        name: 'Richard Hendricks',
+        url: '../resources/logo.png'
+    },
+    {
+        name: 'Erlich Bachman',
+        url: '../resources/logo.png'
+    },
+    {
+        name: 'Monica Hall',
+        url: '../resources/logo.png'
+    },
+    {
+        name: 'Jared Dunn',
+        url: '../resources/logo.png'
+    },
+    {
+        name: 'Dinesh Chugtai',
+        url: '../resources/logo.png'
+    }
+]
+
+const alredyRemoved = []
+let charactersState = db // This fixes issues with updating characters state forcing it to use the current state and not the state that was active when the card was created.
 
 const DashboardCard = () => {
+    const [characters, setCharacters] = useState(db)
+    const [lastDirection, setLastDirection] = useState()
+
+    const childRefs = useMemo(() => Array(db.length).fill(0).map(i => React.createRef()), [])
+
+    const swiped = (direction, nameToDelete) => {
+        console.log('removing: ' + nameToDelete)
+        setLastDirection(direction)
+        alredyRemoved.push(nameToDelete)
+    }
+
+    const outOfFrame = (name) => {
+        console.log(name + ' left the screen!')
+        charactersState = charactersState.filter(character => character.name !== name)
+        setCharacters(charactersState)
+    }
+
+    const swipe = (dir) => {
+        const cardsLeft = characters.filter(person => !alredyRemoved.includes(person.name))
+        if (cardsLeft.length) {
+            const toBeRemoved = cardsLeft[cardsLeft.length - 1].name // Find the card object to be removed
+            const index = db.map(person => person.name).indexOf(toBeRemoved) // Find the index of which to make the reference to
+            alredyRemoved.push(toBeRemoved) // Make sure the next card gets removed next time if this card do not have time to exit the screen
+            childRefs[index].current.swipe(dir) // Swipe the card!
+        }
+    }
+
     return (
-        <div className="tinder">
-
-            <div className="tinder--status">
-                <i className="fa fa-remove"></i>
-                <i className="fa fa-heart"></i>
+        <>
+        <Header/>
+        <div>
+            <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
+            <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
+            <h1>React Tinder Card</h1>
+            <div className='cardContainer'>
+                {characters.map((character, index) =>
+                    <TinderCard ref={childRefs[index]} className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+                        <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+                            <h3>{character.name}</h3>
+                        </div>
+                    </TinderCard>
+                )}
             </div>
-
-            <div className="tinder--card">
-                {/* <img src="https://placeimg.com/600/300/arch"> */}
-                <h3>Demo card 5</h3>
-                <p>This is a demo for Tinder like swipe cards</p>
+            <div className='buttons'>
+                <button onClick={() => swipe('left')}>Swipe left!</button>
+                <button onClick={() => swipe('right')}>Swipe right!</button>
             </div>
-
-            <div className="tinder--buttons">
-                <button id="nope"><i className="fa fa-remove"></i></button>
-                <button id="love"><i className="fa fa-heart"></i></button>
-            </div>
+            {lastDirection ? <h2 key={lastDirection} className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText'>Swipe a card or press a button to get started!</h2>}
         </div>
+        <Footer/>
+        </>
     )
 }
 
