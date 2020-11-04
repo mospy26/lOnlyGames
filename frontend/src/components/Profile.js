d3a2568ddd71db38eb80a9a80475c97190da0c6b
Nabeel Khan <nabeelmahmoodkhan@gmail.com>
2020-11-05 04:20:25 +1100

    profile page draft

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -7,67 +7,84 @@ import { Button, FormGroup, FormControl } from "react-bootstrap";
 
 const Profile = () => {
 
-    const [fName, setFname] = useState()
-    const [lName, setLname] = useState()
-    const [bio, setBio] = useState()
-    const [discord, setDiscord] = useState()
-    const [steam, setSteam] = useState()
+    const [username, setUsername] = useState('mfulwala')
+    const [fName, setFname] = useState('Mustafa')
+    const [lName, setLname] = useState('Fulwala')
+    const [bio, setBio] = useState('This is my bio')
+    const [discord, setDiscord] = useState('discooo')
+    const [steam, setSteam] = useState('steamoo')
     
     return (
         <div>
             <Header />
             <div className='profile_container'>
-                <div className='image_container'>
-                    <img src='https://i.picsum.photos/id/612/200/200.jpg?hmac=HbIkwJ0QBqhSlGTi3bnF4JFTp9BntF-teQZUQhpqWyM' alt='image'></img>
+                <div className='card__container'>
+                    <div className='avatar'>
+                        <img src='https://i.picsum.photos/id/612/200/200.jpg?hmac=HbIkwJ0QBqhSlGTi3bnF4JFTp9BntF-teQZUQhpqWyM' alt='image'></img>
+                    </div>
+                    <div className='username card__info'>{username}</div>
+                    <div className='bio card__info'>
+                        {bio}
+                    </div>
+                    <hr />
+                    <div className='social__ids'>
+                        <div className='id'>{discord}</div>
+                        <div className='id'>{steam}</div>
+                    </div>
                 </div>
                 <div className='form_container'>
                     <form >
-                        <label>First Name</label>
-                        <FormGroup controlId="fName">
-                        <FormControl
-                            autoFocus
-                            type="text"
-                            value={fName}
-                            onChange={e => setFname(e.target.value.trim())}
-                        />
-                        </FormGroup>
+                        <div className='row inner'>
+                            <label>First Name</label>
+                            <FormGroup className='form__group__profile' controlId="fName">
+                            <FormControl
+                                autoFocus
+                                type="text"
+                                value={fName}
+                                onChange={e => setFname(e.target.value)}
+                            />
+                            </FormGroup>
 
-                        <label>Last Name</label>
-                        <FormGroup controlId="lName">
-                        <FormControl
-                            value={lName}
-                            onChange={e => setLname(e.target.value.trim())}
-                            type="text"
-                        />
-                        </FormGroup>
+                            <label>Last Name</label>
+                            <FormGroup className='form__group__profile' controlId="lName">
+                            <FormControl
+                                value={lName}
+                                onChange={e => setLname(e.target.value)}
+                                type="text"
+                            />
+                            </FormGroup>
+                        </div>
 
-                        <label>Bio</label>
-                        <FormGroup controlId="bio">
-                        <FormControl
-                            value={bio}
-                            onChange={e => setBio(e.target.value.trim())}
-                            type="text"
-                        />
-                        </FormGroup>
+                        <div className='row'>
+                            <label>Bio</label>
+                            <FormGroup className='form__group__profile' controlId="bio">
+                            <FormControl
+                                value={bio}
+                                onChange={e => setBio(e.target.value)}
+                                type="text"
+                            />
+                            </FormGroup>
+                        </div>
 
+                        <div className='row ids__inner'>
                         <label>Discord ID</label>
-                        <FormGroup controlId="discord">
-                        <FormControl
-                            value={discord}
-                            onChange={e => setDiscord(e.target.value.trim())}
-                            type="text"
-                        />
-                        </FormGroup>
+                            <FormGroup className='form__group__profile' controlId="discord">
+                            <FormControl
+                                value={discord}
+                                onChange={e => setDiscord(e.target.value)}
+                                type="text"
+                            />
+                            </FormGroup>
 
-                        <label>Steam ID</label>
-                        <FormGroup controlId="steam">
-                        <FormControl
-                            value={steam}
-                            onChange={e => setSteam(e.target.value.trim())}
-                            type="text"
-                        />
-                        </FormGroup>
-
+                            <label>Steam ID</label>
+                            <FormGroup className='form__group__profile' controlId="steam">
+                            <FormControl
+                                value={steam}
+                                onChange={e => setSteam(e.target.value)}
+                                type="text"
+                            />
+                            </FormGroup>
+                        </div>
 
                         <Button block type="submit" className="save-btn">
                         Save
